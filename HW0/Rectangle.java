d1bf80cacbcdc00dc487e8369f6b1d0b676dc333
Chris Janousek <janousek77@gmail.com>
2018-04-05 16:03:52 -0700

    finished HW0

--- HW0/Rectangle.java
+++ HW0/Rectangle.java
@@ -4,6 +4,7 @@
 
 
 public class Rectangle{
+  // #18
   private int x, y, width, height;
 
   public Rectangle(int x, int y, int width, int height){
@@ -36,6 +37,7 @@ public class Rectangle{
     return "Rectangle[x=" + x + ",y=" + y + ",width=" + width + ",height=" + height + "]";
   }
 
+  // #19
   public Rectangle(Point p, int width, int height){
     this.x = p.x;
     this.y = p.y;
@@ -43,6 +45,7 @@ public class Rectangle{
     this.height = height;
   }
 
+  // #20
   public boolean contains(int x, int y){
     return (x<=this.x+width && x>=this.x && y<=this.y+height && y>=this.y);
   }
@@ -51,20 +54,18 @@ public class Rectangle{
     return contains(p.x, p.y);
   }
 
-  public Rectangle union(Rectangle rect){
-    int newX = Math.min(x, rect.x);
-    int newY = Math.min(y, rect.y);
-    int newWidth = Math.max(x + width, rect.x+rect.width)-newX;
-    int newHeight = Math.max(y + height, rect.y+rect.height)-newY;
-    return new Rectangle(newX, newY, newWidth, newHeight);
+  // #21
+  public Rectangle union(Rectangle rect) {
+      int newX = Math.min(this.x, rect.x);
+      int newY = Math.min(this.y, rect.y);
+      int newWidth = Math.max(rect.x + rect.width, this.x + this.width) - newX;
+      int newHeight = Math.max(rect.y + rect.height, this.y + this.height) - newY;
+      return new Rectangle(newX, newY, newWidth, newHeight);
   }
 
+  // #22
   public Rectangle intersection(Rectangle rect){
-    if(this.contains(rect.x, rect.y)
-    || this.contains(rect.x, rect.y+rect.height)
-    || this.contains(rect.x+rect.width, rect.y)
-    || this.contains(rect.x+rect.width, rect.y+rect.height)
-    || ((x+width>=rect.x && y+height>=rect.y)||(x+width<=rect.x && y+height<=rect.y))){
+    if(x+width>=rect.x && y+height>=rect.y){
       int newX = Math.max(x, rect.x);
       int newY = Math.max(y, rect.y);
       int newWidth = Math.min(x + width - newX, rect.x+rect.width-newX);
--- HW0/Test.java
+++ HW0/Test.java
@@ -8,7 +8,7 @@ public class Test{
     Rectangle two = new Rectangle(0,1,6,5);
     Rectangle three = new Rectangle(new Point(4,4),3,4);
     Rectangle four = new Rectangle(new Point(0,1),6,2);
-    Rectangle five = new Rectangle(new Point(3,0),2,4);
+    Rectangle five = new Rectangle(new Point(3,0),3,4);
     Rectangle six = new Rectangle(new Point(0,0),2,2);
     Rectangle seven = new Rectangle(new Point(0,3),2,2);
     // System.out.println("First rectangle's x coordinate: "+two.getX());
@@ -23,9 +23,9 @@ public class Test{
     // System.out.println("First rectangle's height: "+three.getHeight());
     // System.out.println("Does the rectangle contain point (7,7): "+three.contains(one));
     // System.out.println();
-    System.out.println("Union of both rectangles "+three.union(two));
-    // System.out.println("Intersection of both rectangles "+two.intersection(three));
-    // System.out.println("Intersection of two rectangles where edges only cross "+four.intersection(five));
-    // System.out.println("Where two rectangles don't intersect "+six.intersection(seven));
+    // System.out.println("Union of both rectangles "+three.union(two));
+    System.out.println("Intersection of both rectangles "+two.intersection(three));
+    System.out.println("Intersection of two rectangles where edges only cross "+four.intersection(five));
+    System.out.println("Where two rectangles don't intersect "+six.intersection(seven));
   }
 }
